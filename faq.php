3659c94446b3b2a439b81138ac8344dcf8879f7e
a.ankit <a.ankit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 16:17:36 +0000

    support forum link
    git-svn-id: https://plugins.svn.wordpress.org/spoontalk-event-calendar/trunk@1016801 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- faq.php
+++ faq.php
@@ -8,8 +8,9 @@
 
 Use ShortCode <strong>[st_event] </strong>to insert event calendar into a Page / Post. <br><br> For best results embed the calendar in a fullwidth Page / Post
 
+<br><br>
 
-
+We are currently under Beta. Provide suggestions / feedback on the <a href = "https://wordpress.org/support/plugin/spoontalk-event-calendar">Support Forum</a>
 </body>
 
 </html>
